c35ab7580c9347f0bd35b9a40c6106fd92ec1581
lenz1990 <lenz9019@gmail.com>
2026-01-14 18:29:07 +0100

    Update me.js

--- functions/api/me.js
+++ functions/api/me.js
@@ -2,19 +2,8 @@ import { json, getUserFromSession } from "../_shared.js";
 
 export async function onRequestGet(context) {
   const user = await getUserFromSession(context.env, context.request);
-
-  // Nicht eingeloggt => klarer 401, ok:false
-  if (!user) {
-    return json({ ok: false, user: null }, { status: 401 });
-  }
-
-  // Eingeloggt => ok:true + user
   return json({
     ok: true,
-    user: {
-      id: user.id,
-      username: user.username,
-      is_admin: user.is_admin,
-    },
+    user: user ? { id: user.id, username: user.username, is_admin: user.is_admin } : null,
   });
 }
